4d85fe2a5fc9a40c6c8d28470b57c0c9a31e212d
Israel Adewuyi <isistickz@gmail.com>
2025-10-03 01:09:02 +0000

    implemented computing entropy

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import torch
 import logging
 from torch import Tensor
 from jaxtyping import Float, Int
-
+import torch.nn.functional as F
 from datasets import load_dataset, Dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
@@ -14,7 +14,32 @@ logger = logging.getLogger(__name__)
 def load_hf_dataset() -> Dataset:
     dataset = load_dataset("israel-adewuyi/eval_data_alphabet_sort", split="train")
 
-    return list(dataset["prompt"])[:64]
+    return list(dataset["prompt"])[:32]
+
+def compute_entropy(
+    logits: Float[Tensor, "batch seq_len vocab_size"],
+    labels: Int[Tensor, "batch seq_len"]
+) -> float:
+    """
+    Computes the entropy across the unembedding of a LM
+    """
+    shift_logits = logits[:, :-1, :].contiguous()
+    shift_labels = labels[:, 1:].contiguous()
+    
+    valid_mask = (shift_labels != -100)
+    
+    log_softmax = F.log_softmax(shift_logits, dim=-1)
+    softmax_probs = torch.exp(log_softmax)
+    
+    # Per-position entropy
+    per_position_entropy = -(softmax_probs * log_softmax).sum(dim=-1)
+    
+    # Mask and average
+    num_valid_tokens = valid_mask.sum().item()
+    masked_entropy = per_position_entropy * valid_mask.float()
+    avg_entropy = torch.sum(masked_entropy) / num_valid_tokens
+    
+    return avg_entropy.item()
 
 def compute_perplexity(
     logits: Float[Tensor, "batch seq_len vocab_size"], 
@@ -81,15 +106,19 @@ def run_inference(
     
     with torch.no_grad():
         outputs = model(**inputs)
+        
+    del inputs
+    del input_ids
     
     logits = outputs.logits
-    logger.debug(f"Logits: {logits}")
     logger.debug(f"Logits shape: {logits.shape}")
     
     perplexity = compute_perplexity(logits, labels)
-    logger.info(f"Perplexity: {perplexity}")
+    logger.debug(f"Perplexity: {perplexity}")
     
-    return perplexity
+    entropy = compute_entropy(logits, labels)
+    
+    return perplexity, entropy
 
 def save_perplexities(perplexities: list, model_names: list) -> None:
     os.makedirs("artefacts", exist_ok=True)
